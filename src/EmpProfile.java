8c091417140f80a180864d7b3445791b8b72839b
alex_geo <a.georgoudaki@gmail.com>
2013-01-25 22:51:05 +0100

    v1_before_ns

---
+++ src/EmpProfile.java
@@ -0,0 +1,14 @@
+
+/**
+ *
+ * @author ALEX
+ */
+public class EmpProfile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
