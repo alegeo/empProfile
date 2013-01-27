c4a7283fe64c04dac7003ee435ed95c701f2a5b3
sony <sony@130.229.172.166>
2013-01-27 08:04:58 +0800

    delia_3

--- src/SampleXSLTTransformer.java
+++ src/SampleXSLTTransformer.java
@@ -13,9 +13,9 @@ public class SampleXSLTTransformer {
 		try {
 			TransformerFactory  tFactory =  TransformerFactory.newInstance();
    	
-			Source xslSource = new StreamSource( "src/myXSD/MyCV.xsl" );
+			Source xslSource = new StreamSource( "src/xslt.xsl" );
 			Transformer transformer = tFactory.newTransformer( xslSource );
-			transformer.transform( new StreamSource( "Transcript.xml" ),new StreamResult( new FileOutputStream( "test_output.xml" )));
+			transformer.transform( new StreamSource( "src/cv.xml" ),new StreamResult( new FileOutputStream( "test_output.xml" )));
 
 		}catch(Exception ex) {
 			 ex.printStackTrace();
