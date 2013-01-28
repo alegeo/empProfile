f4a23cdf828e1fe8cf1449538aec0b3ccca3e424
alex_geo <a.georgoudaki@gmail.com>
2013-01-28 17:01:49 +0100

    company info

---
+++ src/empprofile/XSLTTransformer.java
@@ -0,0 +1,26 @@
+package empprofile;
+
+import java.io.*;
+
+
+import javax.xml.transform.*;
+import javax.xml.transform.stream.*;
+
+public class XSLTTransformer {
+
+	
+	public static void main(String[] args) {
+		try {
+			TransformerFactory  tFactory =  TransformerFactory.newInstance();
+   	
+			Source xslSource = new StreamSource( "src/empprofile/xslt.xsl" );
+			Transformer transformer = tFactory.newTransformer( xslSource );
+			transformer.transform( new StreamSource( "src/empprofile/cv.xml" ),new StreamResult( new FileOutputStream( "test_output.xml" )));
+
+		}catch(Exception ex) {
+			 ex.printStackTrace();
+		}
+
+	}
+
+}
